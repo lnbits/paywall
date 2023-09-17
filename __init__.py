564f750eef47b93e0a0265e66657623ee958b5fe
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-09-17 11:03:26 +0100

    allow custom path

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
@@ -11,14 +10,13 @@ paywall_ext: APIRouter = APIRouter(prefix="/paywall", tags=["Paywall"])
 paywall_static_files = [
     {
         "path": "/paywall/static",
-        "app": StaticFiles(directory="lnbits/extensions/paywall/static"),
         "name": "paywall_static",
     }
 ]
 
 
 def paywall_renderer():
-    return template_renderer(["lnbits/extensions/paywall/templates"])
+    return template_renderer(["paywall/templates"])
 
 
 from .views import *  # noqa: F401,F403
