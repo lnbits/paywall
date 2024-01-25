577eaf1bf4a87c1cf2f024846b867761178be0fa
Vlad Stan <stan.v.vlad@gmail.com>
2024-01-25 14:05:21 +0200

    feat: show edit dialog

--- models.py
+++ models.py
@@ -52,5 +52,5 @@ class Paywall(BaseModel):
     def from_row(cls, row: Row) -> "Paywall":
         data = dict(row)
         data["remembers"] = bool(data["remembers"])
-        data["extras"] = json.loads(data["extras"]) if data["extras"] else None
+        data["extras"] = PaywallConfig(**json.loads(data["extras"])) if data["extras"] else None
         return cls(**data)
